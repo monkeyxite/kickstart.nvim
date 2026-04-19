fd627e627f19a418c77b617873a1a9f386cd930b
Jonny Hou <jonny.hou@ericsson.com>
2026-04-19 15:16:47 +0200

    set markdown tab width to 4

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,6 +1,6 @@
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
 vim.bo.expandtab = true
 
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
